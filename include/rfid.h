8ca37f59c827f9e3467b16ba36bf712f3913ec72
sSamuel-sSilva <samueloliveiraads1@gmail.com>
2026-05-27 00:46:31 -0300

    refatoracao novamente e retorno do card uuid

--- include/rfid.h
+++ include/rfid.h
@@ -1,21 +1,10 @@
 #ifndef RFID_H
 #define RFID_H
 
-
 #include <SPI.h>
 #include <MFRC522.h>
 
-const int SDA_PIN  = 5;
-const int SCK_PIN  = 18;
-const int MISO_PIN = 19;
-const int MOSI_PIN = 21;
-const int RST_PIN  = 22;
-
-extern MFRC522 mfrc;
-
-void init_rfid();
-bool read_card(char buf[16]);
-// void get_card(char buf[16]);
-
+bool init_rfid();
+char* get_card();
 
 #endif
--- src/rfid.cpp
+++ src/rfid.cpp
@@ -1,10 +1,20 @@
+#include <stdbool.h>
 #include "rfid.h"
 
 
-MFRC522 mfrc(SDA_PIN, RST_PIN);
+static const int SDA_PIN  = 5;
+static const int SCK_PIN  = 18;
+static const int MISO_PIN = 19;
+static const int MOSI_PIN = 21;
+static const int RST_PIN  = 22;
+
+static MFRC522 mfrc(SDA_PIN, RST_PIN);
+static char current_card[16];
+
+#define SERIAL_LOG_ERROR Serial.printf("ERROR in (%s) - [%s: %d]\n", __FILE__, __func__, __LINE__);
 
 
-void init_rfid()
+bool init_rfid()
 {   
   SPI.begin(SCK_PIN, MISO_PIN, MOSI_PIN, SDA_PIN);
   mfrc.PCD_Init();
@@ -12,17 +22,17 @@ void init_rfid()
   byte v = mfrc.PCD_ReadRegister(mfrc.VersionReg);
   if (v == 0x00 || v == 0xFF)
   {
-    Serial.println("rfid erro");
-    return;
+    SERIAL_LOG_ERROR
+    return false;
   }
 
-  Serial.println("rfid ok");
+  return true;
 }
 
 
-bool read_card(char buf[16])
+static bool read_card()
 {
-  buf[0] = '\0';
+  current_card[0] = '\0';
 
   if (!(&mfrc)->PICC_IsNewCardPresent()) return false;
   if (!(&mfrc)->PICC_ReadCardSerial()) return false;
@@ -30,11 +40,18 @@ bool read_card(char buf[16])
   int off = 0;
   for (byte i = 0; i < (&mfrc)->uid.size && off < 31; i++)
   {
-    off += snprintf(buf + off, 32 - off,
+    off += snprintf(current_card + off, 32 - off,
                     (i == (&mfrc)->uid.size - 1) ? "%02X" : "%02X ",
                     (&mfrc)->uid.uidByte[i]);
   }
 
   (&mfrc)->PICC_HaltA();
   return true;
+}
+
+
+char* get_card()
+{
+  if (read_card()) return current_card;
+  return NULL;
 }
